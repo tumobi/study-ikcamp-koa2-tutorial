f486087e6f5dc1c8c1dda6f8f13a17dfc63fc8fa
tumobi <tumobi@163.com>
2018-04-05 08:33:03 +0800

    完成中间件用法

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ const Koa = require('koa')
 const app = new Koa();
 
 app.use(async (ctx, next) => {
+    let stime = Date.now()
     await next()
+    let etime = Date.now()
     ctx.response.type = 'text/html'
     ctx.response.body = '<h1>Hello World</h1>'
+    console.log(`请求地址：${ctx.path}，响应时间：${etime - stime} ms`)
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件1 doSoming')
+    await next()
+    console.log('中间件1 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件2 doSoming')
+    // 如果此处不写 await next()，后面的中间件3将不会执行
+    await next()
+    console.log('中间件2 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件3 doSoming')
+    await next()
+    console.log('中间件3 end')
 })
 
 app.listen(3000, () => {
