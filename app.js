7fbb948212947c7d10cbb535eecf370f49175e38
tumobi <tumobi@163.com>
2018-04-05 09:45:01 +0800

    完成POST/GET请求

--- app.js
+++ app.js
@@ -1,111 +1,50 @@
 const Koa = require('koa')
-// 注意 require('koa-router') 返回的是函数
 const router = require('koa-router')()
-const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser')
+
 const app = new Koa()
+app.use(bodyParser())
 
-router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>index page</h1>'
-    // 会继续匹配相同路由并执行
-    await next();
-})
-
-router.all('/', async (ctx, next) => {
-    console.log('match "all" method')
-    await next();
-})
-
+// 请求参数放在 url ？ 后面
 router.get('/home', async (ctx, next) => {
-    ctx.response.body = '<h1>home page</h1>'
+    // 访问 /home?name=tumobi&age=30&age=40
+    console.log(ctx.request.query) // 输出 { name: 'tumobi', age: [ '30', '40' ] }
+    console.log(ctx.request.querystring) // 输出 name=tumobi&age=30&age=40
+    ctx.body = '<h1>home page</h1>'
 })
 
-router.get('404', '/404', async (ctx, next) => {
-    ctx.status = 404
-    ctx.response.body = '404 Not Found'
+// 请求参数放在 url 中
+router.get('/users/:id/:name', async (ctx, next) => {
+    // 访问 /users/12/tumobi
+    console.log(ctx.params) // 输出 { id: '12', name: 'tumobi' }
+    ctx.body = '<h1>/users/:id/:name page</h1>'
 })
 
-// 其它请求方法
-router
-    .get('/test', async (ctx, next) => {
-        ctx.body = 'get test page!';
-    })
-    .post('/test', async (ctx, next) => {
-        ctx.body = 'post test page!';
-    })
-    .put('/test/:id', async (ctx, next) => {
-        ctx.body = 'put test page!';
-    })
-    .del('/test/:id', async (ctx, next) => {
-        ctx.body = 'del test page!';
-    })
-    .all('/test/:id', async (ctx, next) => {
-        ctx.body = 'all test page!';
-    });
-
-router.all('/*', async (ctx, next) => {
-    console.log('set Access-Control-Allow-Origin')
-    // *代表允许来自所有域名请求
-    ctx.set("Access-Control-Allow-Origin", "*");
-    await next()
+// 请求参数放在 body 中，需要安装 npm i koa-bodyparser -S
+// 增加返回表单页面的路由
+router.get('/login', async (ctx, next) => {
+    ctx.response.body =
+        `
+      <form action="/login" method="post">
+        <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
+        <br/>
+        <input name="password" type="text" placeholder="请输入密码：123456"/>
+        <br/> 
+        <button>GoGoGo</button>
+      </form>
+    `
 })
 
-// 命名路由
-router.get('user', '/users/:id', (ctx, next) => {
-    console.log(ctx.request.url)
-    // 根据路由名生成 url
-    // const url = router.url('user', 3)
-    const url = router.url('user', { id: 3 })
-    console.log(url)
-    ctx.response.body = '<h1>users page</h1>'
-})
-
-// 单个路由多中间件的处理
-router.get(
-    '/multiple/:id',
-    async (ctx, next) => {
-        // 首先读取用户的信息，异步操作
-        ctx.state.user = { name: 'tumobi', age: 30 }
-        next()
-    },
-    async (ctx, next) => {
-        console.log(ctx.state.user)
-        ctx.body = 'multiple page'
+// 增加响应表单请求的路由
+router.post('/login', async (ctx, next) => {
+    let { name, password } = ctx.request.body
+    if (name === 'ikcamp' && password === '123456') {
+        ctx.response.body = `Hello, ${name}`
+    } else {
+        ctx.response.body = '账号信息错误'
     }
-)
-
-// 嵌套路由
-const forums = new Router();
-const posts = new Router();
-posts.get('/', (ctx, next) => {
-    // 匹配路由 /forums/3/posts
-    ctx.body = 'posts page'
 })
-posts.get('/:id', (ctx, next) => {
-    // 匹配路由 /forums/3/posts/2
-    ctx.body = 'posts detail page'
-})
-forums.use('/forums/:fid/posts', posts.routes(), posts.allowedMethods())
-app.use(forums.routes())
 
-// 路由前缀
-const pre = new Router({
-    prefix: '/prefix'
-});
-pre.get('/', (ctx, next) => {ctx.body = 'prefix page' }); // 匹配路由 "/prefix" 
-pre.get('/:id', (ctx, next) => { ctx.body = 'prefix detail page'}); // 匹配路由 "/prefix/:id" 
-app.use(pre.routes())
-
-// url 参数
-router.get('/:category/:title', function (ctx, next) {
-    console.log(ctx.params);
-    // => { category: 'programming', title: 'how-to-node' } 
-});
-
-// 重定向路由，应该放在最后
-router.use((ctx, next) => {
-    // 重定向到 404 页面
-    ctx.redirect(ctx.router.url('404'))
-})
 
 app.use(router.routes())
 
